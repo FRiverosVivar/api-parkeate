83457de86c40c63595c8f157e857f0d6d3701b10
friveros98 <francoriverosvivar@gmail.com>
2023-07-16 01:55:11 -0400

    no message

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,7 @@ async function bootstrap() {
   );
   app.enableShutdownHooks();
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  await app.listen(8080);
   if (module.hot) {
     module.hot.accept();
     module.hot.dispose(() => app.close());
