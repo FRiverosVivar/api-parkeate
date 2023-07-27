427a4f4a699804079bcf8cc95e96fc4222f1d164
friveros98 <francoriverosvivar@gmail.com>
2023-07-27 00:08:24 -0400

    no message

--- aws/scripts/application-start.sh
+++ aws/scripts/application-start.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 set -xe
-if [ "$APPLICATION_NAME" == "api-dev" ]
+app = $(/opt/elasticbeanstalk/bin/get-config environment -k APPLICATION_NAME)
+if [ app == "api-dev" ]
 then
   npm run start:development
 fi
-if [ "$APPLICATION_NAME" == "api-staging" ]
+if [ app == "api-staging" ]
 then
     npm run start:staging
 fi
-if [ "$APPLICATION_NAME" == "api-prod" ]
+if [ app == "api-prod" ]
 then
   npm run start:prod
 fi
