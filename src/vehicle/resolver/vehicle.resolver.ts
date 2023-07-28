fc5b7b61b4b96bb139cdf1234f8e43090b722539
friveros98 <francoriverosvivar@gmail.com>
2023-07-28 10:12:42 -0400

    no message

--- src/vehicle/resolver/vehicle.resolver.ts
+++ src/vehicle/resolver/vehicle.resolver.ts
@@ -5,8 +5,14 @@ import { UpdateVehicleInput } from "../model/update-vehicle.input";
 import { CreateVehicleInput } from "../model/create-vehicle.input";
 import { UserService } from "../../user/service/user.service";
 import { map } from "rxjs";
+import { UseGuards } from "@nestjs/common";
+import { JwtAuthGuard } from "../../auth/guards/jwt-auth.guard";
+import { UserTypeGuard } from "../../auth/guards/user-type.guard";
+import { CurrentUser } from "../../auth/decorator/current-user.decorator";
+import { UserEntity } from "../../user/entity/user.entity";
 
 @Resolver(VehicleEntity)
+@UseGuards(JwtAuthGuard)
 export class VehicleResolver {
   constructor(
     private readonly vehicleService: VehicleService,
@@ -19,10 +25,6 @@ export class VehicleResolver {
   ) {
     return this.vehicleService.createVehicle(createVehicleInput, ownerId)
   }
-  @Query(() => [VehicleEntity], { name: 'findVehicleByUserId' })
-  findVehicleByUserId(@Args('userId', { type: () => String }) userId: string) {
-    return this.vehicleService.findVehiclesByUserId(userId);
-  }
   @Query(() => VehicleEntity, { name: 'findVehicleById' })
   findVehicleById(@Args('vehicleId', { type: () => String }) vehicleId: string) {
     return this.vehicleService.findVehicleById(vehicleId);
@@ -34,5 +36,9 @@ export class VehicleResolver {
   @Mutation(() => VehicleEntity)
   removeVehicle(@Args('vehicleId') vehicleId: string) {
     return this.vehicleService.removeVehicle(vehicleId);
+  }
+  @Query(() => VehicleEntity, { name: 'findVehiclesByUserId' })
+  findVehiclesByUserId(@CurrentUser() user: UserEntity) {
+    return this.vehicleService.findVehiclesByUserId(user.id);
   }
 }
