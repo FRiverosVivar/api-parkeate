d119e2d25a9df1b589ab84ef04c3023222a688a4
Franco Riveros <francoriverosvivar@gmail.com>
2023-10-29 20:18:36 -0300

    no message

--- src/coupons/model/create-coupon.input.ts
+++ src/coupons/model/create-coupon.input.ts
@@ -6,24 +6,26 @@ import { CouponsUseEnum } from "../constants/coupons-use.enum";
 @InputType()
 @ArgsType()
 export class CreateCouponInput {
-    @Field(() => String)
-    code: string
-    @Field(() => String)
-    createdBy: string
-    @Field(() => Int)
-    type: CouponsTypeEnum
-    @Field(() => Int)
-    behavior: CouponsBehaviorEnum
-    @Field(() => Int)
-    use: CouponsUseEnum
-    // @Field(() => [String])
-    // assignedUsers: string[]
-    @Field(() => Boolean)
-    valid: boolean
-    @Field(() => Boolean)
-    active: boolean
-    @Field(() => Date, {nullable: true})
-    dateStart: Date
-    @Field(() => Date, {nullable: true})
-    dateEnd: Date
+  @Field(() => String, { nullable: true })
+  code: string;
+  @Field(() => String)
+  createdBy: string;
+  @Field(() => Int)
+  type: CouponsTypeEnum;
+  @Field(() => Int)
+  behavior: CouponsBehaviorEnum;
+  @Field(() => Int)
+  use: CouponsUseEnum;
+  // @Field(() => [String])
+  // assignedUsers: string[]
+  @Field(() => Boolean)
+  valid: boolean;
+  @Field(() => Boolean)
+  active: boolean;
+  @Field(() => Date, { nullable: true })
+  dateStart: Date;
+  @Field(() => Date, { nullable: true })
+  dateEnd: Date;
+  @Field(() => Int, { nullable: true })
+  useTimes: number;
 }
