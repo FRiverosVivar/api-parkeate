e9dd5c57681e467c2b365920cf6e6d085124563e
Franco Riveros <francoriverosvivar@gmail.com>
2023-11-23 01:20:34 -0300

    no message

--- src/parking/entity/parking.entity.ts
+++ src/parking/entity/parking.entity.ts
@@ -1,5 +1,15 @@
 import { Field, Float, Int, ObjectType } from "@nestjs/graphql";
-import { Column, Entity, Index, JoinColumn, JoinTable, ManyToMany, ManyToOne, OneToMany, Point } from "typeorm";
+import {
+  Column,
+  Entity,
+  Index,
+  JoinColumn,
+  JoinTable,
+  ManyToMany,
+  ManyToOne,
+  OneToMany,
+  Point,
+} from "typeorm";
 import { BaseEntityWithIdAbstract } from "../../utils/interfaces/base-entity-with-id.abstract";
 import { UserEntity } from "../../user/entity/user.entity";
 import { ClientEntity } from "../../client/entity/client.entity";
@@ -11,80 +21,84 @@ import { GeometryGQL } from "../scalar/point.scalar";
 import { Geometry } from "geojson";
 import { ParkingType } from "../model/parking-type.enum";
 
-@Entity('parking')
+@Entity("parking")
 @ObjectType()
-export class ParkingEntity extends BaseEntityWithIdAbstract{
+export class ParkingEntity extends BaseEntityWithIdAbstract {
   @Column()
   @Field(() => Boolean)
-  active: boolean
+  active: boolean;
   @Column()
   @Field(() => Boolean)
-  reserved: boolean
+  reserved: boolean;
   @Column()
   @Field(() => String)
-  name: string
+  name: string;
   @Column()
   @Field(() => Boolean)
-  blocked: boolean
+  blocked: boolean;
   @Column()
   @Field(() => String)
-  address: string
+  address: string;
   @Column()
   @Field(() => Int)
-  floor: number
+  floor: number;
   @Column()
   @Field(() => String)
-  section: string
+  section: string;
   @Column()
   @Field(() => String)
-  code: string
+  code: string;
   @Column()
   @Field(() => Int)
-  type: ParkingType
-  @Column({nullable: true})
-  @Field(() => String, {nullable: true})
-  photo: string
+  type: ParkingType;
+  @Column({ nullable: true })
+  @Field(() => String, { nullable: true })
+  photo: string;
   @ManyToMany(() => UserEntity, (u) => u.restrictedParkings)
-  @JoinTable(
-    {
-      name: 'blocked_user_parkings',
-      joinColumn: {
-        name: "parkingId",
-        referencedColumnName: "id"
-      },
-      inverseJoinColumn: {
-        name: "userId",
-        referencedColumnName: "id"
-      }
-    }
-  )
-  @Field(() =>[UserEntity])
-  blockedUsers: UserEntity[]
+  @JoinTable({
+    name: "blocked_user_parkings",
+    joinColumn: {
+      name: "parkingId",
+      referencedColumnName: "id",
+    },
+    inverseJoinColumn: {
+      name: "userId",
+      referencedColumnName: "id",
+    },
+  })
+  @Field(() => [UserEntity])
+  blockedUsers: UserEntity[];
   @Column()
   @Field(() => String)
-  tax: string
+  tax: string;
   @Column()
   @Field(() => String)
-  pricePerMinute: string
+  pricePerMinute: string;
   @Column()
   @Field(() => String)
-  priceMonthly: string
+  priceMonthly: string;
   @Column()
   @Field(() => String)
-  priceYearly: string
-  @ManyToOne(() => ClientEntity, (c) => c.parkingList, {eager: true})
+  priceYearly: string;
+  @ManyToOne(() => ClientEntity, (c) => c.parkingList, { eager: true })
   @Field(() => ClientEntity)
-  client: ClientEntity
-  @OneToMany(() => ScheduleEntity, (p) => p.parking, {eager: true, nullable: true})
+  client: ClientEntity;
+  @OneToMany(() => ScheduleEntity, (p) => p.parking, {
+    eager: true,
+    nullable: true,
+  })
   @Field(() => [ScheduleEntity])
   schedule?: ScheduleEntity[];
   @ManyToOne(() => BuildingEntity, (b) => b.parkingList)
   @Field(() => BuildingEntity)
-  building: BuildingEntity
+  building: BuildingEntity;
   @OneToMany(() => BookingEntity, (b) => b.parking)
   @Field(() => [BookingEntity])
-  bookings: BookingEntity[]
-  @Column({nullable: true})
-  @Field(() => String, {nullable: true})
+  bookings: BookingEntity[];
+  @Column({ nullable: true })
+  @Field(() => String, { nullable: true })
   phone: string;
+  @Column({ nullable: true })
+  @Field(() => String, { nullable: true })
+  contactEmail: string;
 }
