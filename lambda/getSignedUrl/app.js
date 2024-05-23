f7308a5118af517e3e61818a6a323f015ba9fb97
Franco Riveros <francoriverosvivar@gmail.com>
2024-05-23 03:14:09 -0400

    no message

---
+++ lambda/getSignedUrl/app.js
@@ -0,0 +1,26 @@
+import { S3Client, PutObjectCommand  } from '@aws-sdk/client-s3';
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+
+const s3Client = new S3Client({ region: "sa-east-1" })
+export const lambdaHandler = async (event) => {
+
+  const folderName = event.pathParameters.folder;
+  const directorypath = event.pathParameters.item;
+
+  const command = new PutObjectCommand({
+    Bucket: folderName,
+    Key: directorypath
+  });
+  const signedUrl = await getSignedUrl(s3Client, command, {
+    expiresIn: 300
+  });
+
+  const response = {
+    signedUrl,
+    directorypath,
+  }
+  return {
+    statusCode: 200,
+    body: signedUrl
+  }
+};
