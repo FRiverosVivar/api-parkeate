bc1b686b8f6f6378fb5bb0e3412a1a6629f92456
Franco Riveros <francoriverosvivar@gmail.com>
2024-05-29 01:15:35 -0400

    no message

--- src/requests/enum/request-status.enum.ts
+++ src/requests/enum/request-status.enum.ts
@@ -1,14 +1,12 @@
 export enum RequestStatusEnum {
   PENDING_SEND_FORM,
   PENDING_SEND_CALENDAR,
-  READY_TO_FINISH,
   FINISHED,
   CANCELED
 }
 export const RequestStatusNames = [
-  "Pendiente de envío de formulario",
+  "Formulario por Completar",
   "Pendiente de reunión con equipo",
-  "Listo para terminar",
   "Finalizado",
   "Cancelada",
 ]
@@ -16,7 +14,6 @@ export const RequestStatusNames = [
 export const RequestStatus = [
   RequestStatusEnum.PENDING_SEND_FORM,
   RequestStatusEnum.PENDING_SEND_CALENDAR,
-  RequestStatusEnum.READY_TO_FINISH,
   RequestStatusEnum.FINISHED,
   RequestStatusEnum.CANCELED
 ]
